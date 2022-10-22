d3937adef8a64a51099a9bb8cb70c63ea5db1994
aLauer <116307765+anlauer@users.noreply.github.com>
2022-10-22 19:55:42 +0200

    Revert "Mod"
    This reverts commit 5e583073fa1e10fa3868572f9a16184af47aff0e.

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -3,4 +3,3 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 2!");
 Console.WriteLine("Hello, World 3!");
 Console.WriteLine("Hello, World 4 dev");
-Console.WriteLine("Hello, World 4 dev");
