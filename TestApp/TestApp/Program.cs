0acae3dbabaabfbac778d556898db23cc8624511
aLauer <116307765+anlauer@users.noreply.github.com>
2022-10-22 22:04:28 +0200

    commit 3

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 2!");
-Console.WriteLine("Hello, World 3!");
-Console.WriteLine("Hello, World 4 dev");
+Console.WriteLine("Hello, World 3.1!");
 Console.WriteLine("Hello, World 5 dev");
+Console.WriteLine("Hello, World 6 dev");
