afd8f5844f5bb64dc1ebf4c447a6aa9f10ec962c
aLauer <116307765+anlauer@users.noreply.github.com>
2022-10-22 22:15:51 +0200

    merge

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 2!");
-Console.WriteLine("Hello, World 3.1!");
-Console.WriteLine("Hello, World 5 dev");
-Console.WriteLine("Hello, World 6 dev");
